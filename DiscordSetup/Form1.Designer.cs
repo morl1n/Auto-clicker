5c3a64a6570c525b9658a6ab5cd3095c276a9683
morl1n <75917062+morl1n@users.noreply.github.com>
2021-04-14 13:18:41 -0300

    ⠀⠀⠀⠀⠀

---
+++ DiscordSetup/Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿
+namespace DiscordSetup
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.siticoneTrackBar1 = new Siticone.UI.WinForms.SiticoneTrackBar();
+            this.siticoneTrackBar2 = new Siticone.UI.WinForms.SiticoneTrackBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.siticoneButton1 = new Siticone.UI.WinForms.SiticoneButton();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.AC = new System.Windows.Forms.Timer(this.components);
+            this.siticoneTransparentDrag1 = new Siticone.UI.WinForms.SiticoneTransparentDrag();
+            this.siticoneButton2 = new Siticone.UI.WinForms.SiticoneButton();
+            this.ant = new System.Windows.Forms.Timer(this.components);
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(166, 99);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(15, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "O";
+            // 
+            // siticoneTrackBar1
+            // 
+            this.siticoneTrackBar1.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(193)))), ((int)(((byte)(200)))), ((int)(((byte)(207)))));
+            this.siticoneTrackBar1.HoveredState.Parent = this.siticoneTrackBar1;
+            this.siticoneTrackBar1.IndicateFocus = false;
+            this.siticoneTrackBar1.Location = new System.Drawing.Point(26, 74);
+            this.siticoneTrackBar1.Maximum = 21;
+            this.siticoneTrackBar1.Name = "siticoneTrackBar1";
+            this.siticoneTrackBar1.Size = new System.Drawing.Size(300, 23);
+            this.siticoneTrackBar1.TabIndex = 0;
+            this.siticoneTrackBar1.ThumbColor = System.Drawing.Color.Red;
+            this.siticoneTrackBar1.Value = 1;
+            this.siticoneTrackBar1.Scroll += new System.Windows.Forms.ScrollEventHandler(this.siticoneTrackBar1_Scroll);
+            // 
+            // siticoneTrackBar2
+            // 
+            this.siticoneTrackBar2.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(193)))), ((int)(((byte)(200)))), ((int)(((byte)(207)))));
+            this.siticoneTrackBar2.HoveredState.Parent = this.siticoneTrackBar2;
+            this.siticoneTrackBar2.IndicateFocus = false;
+            this.siticoneTrackBar2.Location = new System.Drawing.Point(22, 120);
+            this.siticoneTrackBar2.Maximum = 21;
+            this.siticoneTrackBar2.Name = "siticoneTrackBar2";
+            this.siticoneTrackBar2.Size = new System.Drawing.Size(300, 23);
+            this.siticoneTrackBar2.TabIndex = 3;
+            this.siticoneTrackBar2.ThumbColor = System.Drawing.Color.Red;
+            this.siticoneTrackBar2.Value = 1;
+            this.siticoneTrackBar2.Scroll += new System.Windows.Forms.ScrollEventHandler(this.siticoneTrackBar2_Scroll);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(166, 146);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(15, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "O";
+            // 
+            // siticoneButton1
+            // 
+            this.siticoneButton1.BorderRadius = 10;
+            this.siticoneButton1.CheckedState.Parent = this.siticoneButton1;
+            this.siticoneButton1.CustomImages.Parent = this.siticoneButton1;
+            this.siticoneButton1.FillColor = System.Drawing.Color.Red;
+            this.siticoneButton1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.siticoneButton1.ForeColor = System.Drawing.Color.White;
+            this.siticoneButton1.HoveredState.Parent = this.siticoneButton1;
+            this.siticoneButton1.Location = new System.Drawing.Point(87, 190);
+            this.siticoneButton1.Name = "siticoneButton1";
+            this.siticoneButton1.PressedColor = System.Drawing.Color.Empty;
+            this.siticoneButton1.ShadowDecoration.Parent = this.siticoneButton1;
+            this.siticoneButton1.Size = new System.Drawing.Size(180, 45);
+            this.siticoneButton1.TabIndex = 5;
+            this.siticoneButton1.Text = "Start";
+            this.siticoneButton1.Click += new System.EventHandler(this.siticoneButton1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Red;
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.ForeColor = System.Drawing.Color.Black;
+            this.panel1.Location = new System.Drawing.Point(-29, -3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(812, 32);
+            this.panel1.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(736, 7);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(26, 25);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "X";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // AC
+            // 
+            this.AC.Tick += new System.EventHandler(this.AC_Tick);
+            // 
+            // siticoneTransparentDrag1
+            // 
+            this.siticoneTransparentDrag1.DragEndTransparencyValue = 1D;
+            this.siticoneTransparentDrag1.DragStartTransparencyValue = 0.9D;
+            this.siticoneTransparentDrag1.TargetDragControl = this.panel1;
+            // 
+            // siticoneButton2
+            // 
+            this.siticoneButton2.CheckedState.Parent = this.siticoneButton2;
+            this.siticoneButton2.CustomImages.Parent = this.siticoneButton2;
+            this.siticoneButton2.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.siticoneButton2.ForeColor = System.Drawing.Color.White;
+            this.siticoneButton2.HoveredState.Parent = this.siticoneButton2;
+            this.siticoneButton2.Location = new System.Drawing.Point(490, 98);
+            this.siticoneButton2.Name = "siticoneButton2";
+            this.siticoneButton2.ShadowDecoration.Parent = this.siticoneButton2;
+            this.siticoneButton2.Size = new System.Drawing.Size(180, 45);
+            this.siticoneButton2.TabIndex = 7;
+            this.siticoneButton2.Text = "siticoneButton2";
+            // 
+            // ant
+            // 
+            this.ant.Tick += new System.EventHandler(this.ant_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(734, 480);
+            this.Controls.Add(this.siticoneButton2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.siticoneButton1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.siticoneTrackBar2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.siticoneTrackBar1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Ac";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private Siticone.UI.WinForms.SiticoneTrackBar siticoneTrackBar1;
+        private Siticone.UI.WinForms.SiticoneTrackBar siticoneTrackBar2;
+        private System.Windows.Forms.Label label2;
+        private Siticone.UI.WinForms.SiticoneButton siticoneButton1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Timer AC;
+        private System.Windows.Forms.Label label3;
+        private Siticone.UI.WinForms.SiticoneTransparentDrag siticoneTransparentDrag1;
+        private Siticone.UI.WinForms.SiticoneButton siticoneButton2;
+        private System.Windows.Forms.Timer ant;
+    }
+}
+
---
+++ DiscordSetup/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace DiscordSetup
+{
+
+    public partial class Form1 : Form
+    {
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetForegroundWindow();
+        [DllImport("user32.dll")]
+        private static extern bool PostMessage(IntPtr hWnd, uint Msg, int wParam, int lParam);
+        [DllImport("user32", CallingConvention = CallingConvention.StdCall, CharSet = CharSet.Auto)]
+        public static extern bool RegisterHotKey(IntPtr hWnd, int id, int fsModifiers, int vlc);
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern int GetWindowThreadProcessId(IntPtr intptr_0, out int int_0);
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern int GetWindowTextLength(IntPtr intptr_0);
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern int GetWindowText(IntPtr intptr_0, StringBuilder stringBuilder_0, int int_0);
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private string method_0()
+        {
+            string result = string.Empty;
+            IntPtr foregroundWindow = Form1.GetForegroundWindow();
+            int num = Form1.GetWindowTextLength(foregroundWindow) + 1;
+            StringBuilder stringBuilder = new StringBuilder(num);
+            if (Form1.GetWindowText(foregroundWindow, stringBuilder, num) > 0)
+            {
+                result = stringBuilder.ToString();
+            }
+            return result;
+        }
+
+        private void siticoneTrackBar1_Scroll(object sender, ScrollEventArgs e)
+        {
+            label1.Text = siticoneTrackBar1.Value.ToString();
+        }
+
+        private void siticoneTrackBar2_Scroll(object sender, ScrollEventArgs e)
+        {
+            label2.Text = siticoneTrackBar2.Value.ToString();
+        }
+
+        private void AC_Tick(object sender, EventArgs e)
+        {
+            if ((this.method_0().Contains("Minecraft") || this.method_0().Contains("Badlion") || this.method_0().Contains("Lunar Client") || this.method_0().Contains("Labymod") || this.method_0().Contains("OCMC") || this.method_0().Contains("Cheatbreaker") || this.method_0().Contains("J3Ultimate")))
+            {
+                bool flag = Control.MouseButtons == MouseButtons.Left;
+                int interval = 1000 / this.siticoneTrackBar1.Value - 2;
+                if (flag)
+                {
+                    IntPtr foregroundWindow = Form1.GetForegroundWindow();
+                    Form1.PostMessage(foregroundWindow, 513u, 0, 0);
+                    Form1.PostMessage(foregroundWindow, 514u, 0, 0);
+                    this.AC.Interval = interval;
+                }
+            }
+        }
+        private void label3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void siticoneButton1_Click(object sender, EventArgs e)
+        {
+            bool flag = this.siticoneButton1.Text == "OFF";
+            if (flag)
+            {
+                this.siticoneButton1.Text = "ON";
+                this.siticoneButton1.BackColor = Color.FromArgb(50, 2, 70);
+                AC.Start();
+            }
+            else
+            {
+                this.siticoneButton1.Text = "OFF";
+                this.siticoneButton1.BackColor = Color.FromArgb(15, 15, 15);
+                AC.Stop();
+            }
+        }
+    }
+}
